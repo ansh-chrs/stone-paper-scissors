6b97f8439dc0c9b060119ee1517f20a7c2502367
Anshuman <chaurasiaji200@gmail.com>
2023-08-09 10:52:05 +0530

    Add display elements to page and connect them with game logic

--- script.js
+++ script.js
@@ -1,65 +1,52 @@
 const buttons = document.querySelectorAll('button');
+const yourScore = document.querySelector('#playerDisplay');
+const computerScore = document.querySelector('#compDisplay');
+const playerHead =document.querySelector('#playerHeading');
+const compHead =document.querySelector('#compHeading');
 buttons.forEach((btn) => btn.addEventListener('click', game));
 
 let count = 0; //counter variable to count total attempts
-    let compScore = 0;   //score of computer
-    let userScore = 0;   //score of user
+let compScore = 0;   //score of computer
+let userScore = 0;   //score of user
 
 
-    function game(e) {
+function game(e) {
+    let attempt = playRound(e.target.value);
+    ++count;
+    console.log(`round ${count}`);
+    //    console.log(attempt);
+    if (attempt.includes('Win'))
+        userScore++;
 
-    
-      
-        let attempt = playRound(e.target.value);
-        console.log(`round ${count}`);
-               console.log( attempt);
-    
-    
-       // loop to run game attempt 5 times
-    
-        
-            if (attempt.includes('Win'))
-                userScore++;
-    
-            else if (attempt.includes('Loss'))
-                compScore++;
-    
-    
-            else {
-                userScore++;
-                compScore++;
-            }
-    
-            count++;
-    // comparing scores to decide winner
-    
-        console.log(`Your score ${userScore}`);
-        console.log(`Comp Score ${compScore}`)
-          
-         if(count === 5 )
-         {
-        if(userScore > compScore)
-        console.log('You win!!');
-        else if(compScore > userScore)
-        console.log('Comp wins');
-        else console.log('Tie');
-      
-    
-        count = 0;
-        compScore =0;
-        userScore =0;
-         }
-         
+    else if (attempt.includes('Loss'))
+        compScore++;
+    else {
+        userScore++;
+        compScore++;
     }
 
 
+    playerHead.innerText = "YOUR SCORE";
+    compHead.innerText = "COMPUTER SCORE";
+    yourScore.innerText = userScore;
+    computerScore.innerText = compScore;
 
+    if (count === 5) {
+        if (userScore > compScore)
+            console.log('You win!!');
+        else if (compScore > userScore)
+            console.log('Comp wins');
+        else console.log('Tie');
 
-
-
+        count = 0;
+        compScore = 0;
+        userScore = 0;
+    }
+}
 
 
 // function to generate computer value
+
 function getComputerChoice() {
     let num = Math.floor(Math.random() * 3 + 1); //Generates a number between 1 and 3
     if (num === 1) { return 'stone'; }
@@ -67,16 +54,14 @@ function getComputerChoice() {
     else { return 'scissors'; }
 }
 
+
 //function to run a single round of game 
 
 function playRound(playerSelection) {
-
-    let userChoice =playerSelection; 
+    let userChoice = playerSelection;
     let compChoice = getComputerChoice();   //Generatiing computer choice using getComputerChoice function 
-
     if (userChoice === compChoice)
-        return `Tie` ;
-
+        return `Tie`;
     else {
         switch (userChoice) {
 
@@ -98,13 +83,12 @@ function playRound(playerSelection) {
                 else
                     return `Loss ! ${compChoice} beats ${userChoice}`;
         }
-   }
+    }
 }
 
 
 
 
- 
 
 
 
